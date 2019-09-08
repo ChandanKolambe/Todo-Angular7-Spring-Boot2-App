6b4d2e6682c89d530bc3c073c49651fb7fafcb06
ChandanKolambe <chandan.kolambe@knorex.com>
2019-09-08 15:25:21 +0530

    JPA, Hibernate <-> Spring

--- frontend/todo/src/app/todo/todo.component.ts
+++ frontend/todo/src/app/todo/todo.component.ts
@@ -32,7 +32,7 @@ export class TodoComponent implements OnInit {
   }
 
   saveTodo() {
-    if(this.id === -1) {
+    if(this.id == -1) {
       this.todoService.createTodo('user', this.todo)
           .subscribe (
             data => {
--- todo-rest/src/main/java/com/todo/rest/todorest/todo/Todo.java
+++ todo-rest/src/main/java/com/todo/rest/todorest/todo/Todo.java
@@ -1,9 +1,16 @@
 package com.todo.rest.todorest.todo;
 
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Todo {
-	private long id;
+
+	@Id
+	@GeneratedValue
+	private Long id;
 	private String username;
 	private String description;
 	private Date targetDate;
@@ -17,16 +24,16 @@ public class Todo {
 		this.targetDate = targetDate;
 		this.isDone = isDone;
 	}
-	
+
 	protected Todo() {
 
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
